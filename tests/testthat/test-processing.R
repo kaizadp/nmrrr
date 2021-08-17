a6d7a994f0a56fa5dd25a9a6f1f05b37874c69a1
kaizadp <50244730+kaizadp@users.noreply.github.com>
2021-08-17 08:21:07 -0700

    update expect_error and expect_type arguments

--- tests/testthat/test-processing.R
+++ tests/testthat/test-processing.R
@@ -1,13 +1,13 @@
 
 
 test_that("binsets work",{
-  expect_error(set_bins("20"))
+  expect_error(set_bins("Clem"), "too many options!")
 
   clem_old <- read.csv("compdata/Clemente2012_bins.csv")
   clem_new <- set_bins("Clemente2012")
 
   expect_equal(dim(clem_old), dim(clem_new))
 
-  expect_type(clem_new$number, "double")
+  expect_type(clem_new$number, "integer")
 
 })
