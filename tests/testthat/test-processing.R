f5e4ae4488028ae03795b686f128a735bb168bef
kaizadp <50244730+kaizadp@users.noreply.github.com>
2022-02-15 20:43:28 -0800

    update tests. add METHOD = ...

--- tests/testthat/test-processing.R
+++ tests/testthat/test-processing.R
@@ -15,10 +15,10 @@ test_that("binsets work",{
 
 test_that("import-spectra works",{
   # files in correct format
-  expect_error(import_nmr_spectra_data(SPECTRA_FILES = "compdata/spectra-error"),
+  expect_error(import_nmr_spectra_data(SPECTRA_FILES = "compdata/spectra-error", METHOD = "mnova"),
                "no .csv files found!")
 
-  spectra_test = import_nmr_spectra_data(SPECTRA_FILES = "compdata/spectra")
+  spectra_test = import_nmr_spectra_data(SPECTRA_FILES = "compdata/spectra", METHOD = "mnova")
 
   expect_type(spectra_test, "list")
   expect_type(spectra_test$sampleID, "character")
@@ -35,7 +35,7 @@ test_that("binset-assignment works",{
 
   spectra_binsets_old = read.csv("compdata/spectra_binset_test.csv") %>% mutate(sampleID = as.character(sampleID))
 
-  spectra_test = import_nmr_spectra_data(SPECTRA_FILES = "compdata/spectra")
+  spectra_test = import_nmr_spectra_data(SPECTRA_FILES = "compdata/spectra", METHOD = "mnova")
   spectra_binsets_new = assign_compound_classes(dat = spectra_test, BINSET = "Clemente")
 
   expect_equal(dim(spectra_binsets_new), dim(spectra_binsets_old))
