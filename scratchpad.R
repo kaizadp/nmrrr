d8fd74c2e23ed2f9a4e14bf3c70095a6e3fe385b
kaizadp <50244730+kaizadp@users.noreply.github.com>
2022-08-04 12:41:19 -0700

    reorganize testing for `kfp_hysteresis` data

--- scratchpad.R
+++ scratchpad.R
@@ -6,33 +6,59 @@ roxygen2::roxygenise()
 # TESTING NMRRR WORKFLOW --------------------------------------------------
 library(tidyverse)
 
+
+# testing with `kfp_hysteresis` data --------------------------------------
+
+## data generated using MNova
+## spectra files and multi-column peaks files
+
 # 1. process spectra
 #spectra_processed = import_nmr_spectra_data(SPECTRA_FILES = "inst/extdata/spectra") #old code
-spectra_processed = import_nmr_spectra_data(SPECTRA_FILES = "inst/extdata/spectra_mnova", METHOD = "mnova") %>% filter(ppm >= 0 & ppm <= 10)
-spectra_processed_topspin = import_nmr_spectra_data(SPECTRA_FILES = "inst/extdata/spectra_topspin", METHOD = "topspin")
+hyst_spectra_processed = import_nmr_spectra_data(SPECTRA_FILES = "inst/extdata/kfp_hysteresis/spectra_mnova", METHOD = "mnova") %>% filter(ppm >= 0 & ppm <= 10)
 
-spectra_processed_bins = assign_compound_classes(spectra_processed, BINSET = "Clemente")
+hyst_spectra_processed_bins = assign_compound_classes_v2(spectra_processed, BINSET = "Clemente") %>% filter(!is.na(group))
+# hyst_spectra_processed_bins2 = assign_compound_classes(spectra_processed, BINSET = "Clemente")
 
+# spectra graphs (not functions yet)
+hyst_spectra_processed_bins %>%
+  filter(!is.na(group)) %>% filter(group != "NA") %>%
+  ggplot(aes(x = ppm, y = intensity, color = group))+ geom_line()+ facet_wrap(~sampleID) + ylim(0, 5)+
+  scale_x_reverse()
 
 # 2. process peaks
-peaks_processed_mult = process_peaks(PEAKS_FILES = "inst/extdata/peaks_mnova_multiple", METHOD = "multiple columns", BINSET = "Clemente")
+hyst_peaks_processed_mult = process_peaks(PEAKS_FILES = "inst/extdata/kfp_hysteresis/peaks_mnova_multiple", METHOD = "multiple columns")
+hyst_peaks_processed_mult_bins = assign_compound_classes_v2(dat = peaks_processed_mult, BINSET = "Clemente")
+
+# 3. compute relabund - by sample
+hyst_relabund_cores_auc = compute_relabund_cores(DAT = spectra_processed_bins, METHOD = "AUC")
+hyst_relabund_cores_peaks_mult = compute_relabund_cores(DAT = peaks_processed_mult_bins, METHOD = "peaks")
+
+# 4. compute relabund - summary by treatment
+COREKEY = "inst/extdata/kfp_hysteresis/corekey_hyst_KFP.csv"
+TREATMENTS = quos(treatment)
+
+hyst_relabund_summary_auc = compute_relabund_treatments(RELABUND_CORES = relabund_cores_auc, TREATMENTS, COREKEY)
+hyst_relabund_summary_peaks_mult = compute_relabund_treatments(RELABUND_CORES = relabund_cores_peaks_mult, TREATMENTS, COREKEY)
+
+
+
+
+
+
+
+
+spectra_processed_topspin = import_nmr_spectra_data(SPECTRA_FILES = "inst/extdata/spectra_topspin", METHOD = "topspin")
+
+
+
 peaks_processed_sing = process_peaks(PEAKS_FILES = "inst/extdata/peaks_mnova_single", METHOD = "single column", BINSET = "Hertkorn")
 peaks_processed_topspin = process_peaks(PEAKS_FILES = "inst/extdata/peaks_topspin", METHOD = "topspin")
 peaks_processed_topspin2 = assign_compound_classes(peaks_processed_topspin, BINSET = "Clemente")
 
 
 
-# 3. compute relabund - by sample
-relabund_cores_auc = compute_relabund_cores(DAT = spectra_processed_bins, METHOD = "AUC")
-relabund_cores_peaks_mult = compute_relabund_cores(DAT = peaks_processed_mult, METHOD = "peaks")
 relabund_cores_peaks_sing = compute_relabund_cores(DAT = peaks_processed_sing, METHOD = "peaks")
 
-# 4. compute relabund - summary by treatment
-COREKEY = "inst/extdata/corekey_hyst_KFP.csv"
-TREATMENTS = quos(treatment)
-
-relabund_summary_auc = compute_relabund_treatments(RELABUND_CORES = relabund_cores_auc, TREATMENTS, COREKEY)
-relabund_summary_peaks_mult = compute_relabund_treatments(RELABUND_CORES = relabund_cores_peaks_mult, TREATMENTS, COREKEY)
 relabund_summary_peaks_sing = compute_relabund_treatments(RELABUND_CORES = relabund_cores_peaks_sing, TREATMENTS, COREKEY)
 
 relabund_summary_auc %>%
@@ -52,3 +78,23 @@ spectra1 = read.csv("inst/extdata/spectra_topspin/ascii-spec_W300_HB2_Topspin_Sp
 spectra1_bins = assign_compound_classes(spectra1, BINSET = "Clemente")
 spectra1_bins %>% ggplot(aes(x = ppm, y = intensity))+ geom_point()
 ## this keeps only a subset of the data, we need even the non-matches!!!
+
+
+
+
+y = merge(spectra1, bins_dat, by = c(start <= ppm & ppm <= stop))
+
+library(data.table)
+y = foverlaps(spectra1, bins_dat,
+              by.x = "ppm", by.y = c("start", "stop"))
+
+
+
+x = data.table(seq=c("Chr1", "Chr1", "Chr2", "Chr2", "Chr2"),
+               start=c(5,10, 1, 25, 50), end=c(11,20,4,52,60))
+y = data.table(chr=c("Chr1", "Chr1", "Chr2"), start=c(1, 15,1),
+               end=c(4, 18, 55), geneid=letters[1:3])
+setkey(y, chr, start, end)
+foverlaps(x, y, by.x=c("seq", "start", "end"),
+          type="any", which=TRUE)
+
