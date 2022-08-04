9c94d053b0041a1b3130b0cdd063a69d78fc4ddb
kaizadp <50244730+kaizadp@users.noreply.github.com>
2022-08-04 16:09:16 -0700

    reorganize testing for amp and meb data

--- scratchpad.R
+++ scratchpad.R
@@ -5,7 +5,8 @@ roxygen2::roxygenise()
 #
 # TESTING NMRRR WORKFLOW --------------------------------------------------
 library(tidyverse)
-
+source("R/1-functions_processing.R")
+source("R/1-functions_relabund.R")
 
 # testing with `kfp_hysteresis` data --------------------------------------
 
@@ -42,24 +43,53 @@ hyst_relabund_summary_peaks_mult = compute_relabund_treatments(RELABUND_CORES = 
 
 
 
+#
+# testing with `amp_tempest` data -----------------------------------------
+
+## data generated using MNova
+## single-column peaks files
+
+
+# 1. process spectra (?)
+
+
+# 2. process peaks
+tempest_peaks_processed_sing = process_peaks(PEAKS_FILES = "inst/extdata/amp_tempest/peaks_mnova_single", METHOD = "single column")
+tempest_peaks_processed_sing_bins = assign_compound_classes_v2(dat = tempest_peaks_processed_sing, BINSET = "Hertkorn")
+
+
+# 3. compute relabund - by sample
+tempest_relabund_cores_peaks_sing = compute_relabund_cores(DAT = tempest_peaks_processed_sing_bins, METHOD = "peaks")
+# tempest_relabund_summary_peaks_sing = compute_relabund_treatments(RELABUND_CORES = tempest_relabund_cores_peaks_sing, TREATMENTS, COREKEY)
+
+# 4. compute relabund - summary by treatment
+
+
+#
+# testing with `meb_incubation` data --------------------------------------
+
+## data generated using TopSpin
+## spectra and peaks files
+
+# 1. process spectra
+meb_spectra_processed_topspin = import_nmr_spectra_data(SPECTRA_FILES = "inst/extdata/meb_incubation/spectra_topspin", METHOD = "topspin")
+
+# 2. process peaks
+meb_peaks_processed_topspin = process_peaks(PEAKS_FILES = "inst/extdata/meb_incubation/peaks_topspin", METHOD = "topspin")
+meb_peaks_processed_topspin_bins = assign_compound_classes_v2(meb_peaks_processed_topspin, BINSET = "Clemente")
+
+
+# 3. compute relabund - by sample
+meb_relabund_cores_auc = compute_relabund_cores(DAT = meb_peaks_processed_topspin_bins %>% rename(intensity = Intensity), METHOD = "AUC")
+### `meb_relabund_cores_peaks_mult` does not work for topspin data because there is no "area" column
+meb_relabund_cores_peaks_mult = compute_relabund_cores(DAT = meb_peaks_processed_topspin_bins, METHOD = "peaks")
+
+# 4. compute relabund - summary by treatment
 
 
 
+########################
 
-
-spectra_processed_topspin = import_nmr_spectra_data(SPECTRA_FILES = "inst/extdata/spectra_topspin", METHOD = "topspin")
-
-
-
-peaks_processed_sing = process_peaks(PEAKS_FILES = "inst/extdata/peaks_mnova_single", METHOD = "single column", BINSET = "Hertkorn")
-peaks_processed_topspin = process_peaks(PEAKS_FILES = "inst/extdata/peaks_topspin", METHOD = "topspin")
-peaks_processed_topspin2 = assign_compound_classes(peaks_processed_topspin, BINSET = "Clemente")
-
-
-
-relabund_cores_peaks_sing = compute_relabund_cores(DAT = peaks_processed_sing, METHOD = "peaks")
-
-relabund_summary_peaks_sing = compute_relabund_treatments(RELABUND_CORES = relabund_cores_peaks_sing, TREATMENTS, COREKEY)
 
 relabund_summary_auc %>%
   ggplot(aes(x = treatment, y = relabund_mean, fill = group))+
