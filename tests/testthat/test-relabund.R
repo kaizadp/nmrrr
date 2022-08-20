74d4199fbc6b0192c82dedf35afc82660aab7870
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2022-08-20 06:06:37 -0400

    Test code basic cleanup (#13)

--- tests/testthat/test-relabund.R
+++ tests/testthat/test-relabund.R
@@ -1,23 +1,16 @@
 
-
-
 test_that("relabund works", {
-  spectra_binset <- read.csv("compdata/spectra_binset_test.csv") %>% mutate(sampleID = as.character(sampleID))
+  spectra_binset <- read.csv("compdata/spectra_binset_test.csv")
+  spectra_binset$sampleID <- as.character(spectra_binset$sampleID)
 
-
-  expect_error(compute_relabund_cores(
-    DAT = spectra_binset,
-    METHOD = "x"
-  ), "choose correct method")
+  expect_error(compute_relabund_cores(DAT = spectra_binset, METHOD = "x"),
+               regexp = "choose correct method")
   #  expect_error(compute_relabund_cores(DAT = spectra_binset,
   #                                             METHOD = "peaks"), "peaks data needed")
-  expect_type(compute_relabund_cores(
-    DAT = spectra_binset,
-    METHOD = "AUC"
-  ), "list") ## <-- list??
+  expect_type(compute_relabund_cores(DAT = spectra_binset, METHOD = "AUC"),
+              type = "list") ## <-- list??
 
-  expect_named(
-    compute_relabund_cores(DAT = spectra_binset, METHOD = "AUC"),
-    c("sampleID", "group", "relabund")
+  expect_named(compute_relabund_cores(DAT = spectra_binset, METHOD = "AUC"),
+    expected = c("sampleID", "group", "relabund")
   )
 })
