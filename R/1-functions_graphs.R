9a674b9124bd73eae447d21a298d88e081d5b62a
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2022-08-30 03:33:51 -0400

    Remove dplyr from remaining code

--- R/1-functions_graphs.R
+++ R/1-functions_graphs.R
@@ -12,67 +12,60 @@
 #' @param BINSET spectral binning
 #' @param LABEL_POSITION y-axis position for bin labels
 #' @param mapping aes(x = , y = )
-#' @param STAGGER how much to stagger the labels?
+#' @param STAGGER how much to stagger the labels? KP_TO (not clear)
 #'
 #' @return A dataframe with columns describing ... KP_TODO
 #'
 #' @import ggplot2
-#' @importFrom dplyr %>% distinct filter mutate select left_join rename
 #' @importFrom tidyr drop_na replace_na
 #' @importFrom utils head
 #' @export
 gg_spectra <- function(dat, BINSET, LABEL_POSITION, mapping, STAGGER) {
   # Quiet R CMD CHECK notes
-  start <- number <- sampleID <- newsource <- y_factor <-
-    intensity <- intensity_new <- row_number <- NULL
+  start <- number <- sampleID <- newsource <- NULL
 
   bins_dat <- BINSET
 
   # create spectra-base plot ----
+  odds <- bins_dat[seq(1, nrow(bins_dat), by = 2),]
+  evens <- bins_dat[seq(2, nrow(bins_dat), by = 2),]
   spectra_base <-
     ggplot() +
     # stagger bracketing lines for odd vs. even rows
     geom_segment(
-      data = bins_dat %>% filter(row_number() %% 2 == 0),
-      aes(x = start, xend = stop, y = LABEL_POSITION, yend = LABEL_POSITION), color = "black"
+      data = evens,
+      aes(x = start, xend = stop, y = LABEL_POSITION, yend = LABEL_POSITION),
+      color = "black"
     ) +
     geom_segment(
-      data = bins_dat %>% filter(row_number() %% 2 == 1),
-      aes(x = start, xend = stop, y = LABEL_POSITION - 0.2, yend = LABEL_POSITION - 0.2), color = "black"
+      data = odds,
+      aes(x = start, xend = stop, y = LABEL_POSITION - 0.2, yend = LABEL_POSITION - 0.2),
+      color = "black"
     ) +
     # stagger numbering like the lines
     geom_text(
-      data = bins_dat %>% filter(row_number() %% 2 == 0),
+      data = evens,
       aes(x = (start + stop) / 2, y = LABEL_POSITION + 0.1, label = number)
     ) +
     geom_text(
-      data = bins_dat %>% filter(row_number() %% 2 == 1),
+      data = odds,
       aes(x = (start + stop) / 2, y = LABEL_POSITION - 0.1, label = number)
     ) +
     scale_x_reverse(limits = c(10, 0)) +
-
-    # geom_path(data = dat, aes(x = ppm, y = intensity, color = source))+
     xlab("shift, ppm") +
     ylab("intensity")
 
   # add staggering factor ----
 
   stagger_factor <- 1 / STAGGER
-  dat_y_stagger <-
-    dat %>%
-    distinct(sampleID) %>%
-    mutate(newsource = sampleID != c(NA, head(sampleID, -1))) %>%
-    drop_na() %>%
-    mutate(y_factor = cumsum(newsource) / stagger_factor) %>%
-    select(sampleID, y_factor)
+  dat_y_stagger <- weak_tibble(
+    sampleID = unique(dat$sampleID),
+    newsource = c(TRUE, sampleID[-1] != head(sampleID, -1)),
+    y_factor = (cumsum(newsource) - 1) / stagger_factor
+  )
 
-  spectra_new <-
-    dat %>%
-    left_join(dat_y_stagger) %>%
-    replace_na(list(y_factor = 0)) %>%
-    mutate(intensity_new = intensity + y_factor) %>%
-    select(-intensity) %>%
-    rename(intensity = intensity_new)
+  spectra_new <- merge(dat, dat_y_stagger, by = "sampleID")
+  spectra_new$intensity <- spectra_new$intensity + spectra_new$y_factor
 
   # combined plot ----
 
